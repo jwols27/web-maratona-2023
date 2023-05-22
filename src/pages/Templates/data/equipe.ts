6ea930c0d280285aa6270c30ab3f2f166bb90816
Júlia Patricia Wolschick <juliapwols@pm.me>
2023-05-22 15:12:26 -0300

    View de equipe e colaboradores

---
+++ src/pages/Templates/data/equipe.ts
@@ -0,0 +1,25 @@
+import { IPerfilProps } from '../EquipeColaboradores/components/Perfil';
+import zonta from '../../../shared/assets/avatars/zonta.jpg';
+import tabuti from '../../../shared/assets/avatars/tabuti.jpg';
+
+export const equipe: IPerfilProps[] = [
+    {
+        src: zonta,
+        nome: 'Tiago Zonta',
+        instituicao: 'UNOESC',
+        children:
+            'Diretor da 28ª Final Brasileira da Maratona SBC de Programação'
+    },
+    {
+        src: tabuti,
+        nome: 'Lucy Mari Tabuti',
+        instituicao: 'Instituto Criativo',
+        children: 'Diretora de patrocínios da Maratona SBC de Programação'
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+];
